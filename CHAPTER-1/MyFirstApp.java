1404656eb4984c063460c75fa4adabceba0e0d8e
Surya Moharana <suryachintu1994@gmail.com>
2022-10-27 14:35:59 +0530

    HEAD_FIRST_JAVA
    Exercises of CHAPTER-1 TO CHAPTER-4 ADDED

---
+++ CHAPTER-1/MyFirstApp.java
@@ -0,0 +1,7 @@
+class MyFirstApp{
+	public static void main(String[] args) {
+
+		System.out.println("Surya is a KING");
+		
+	}
+}
---
+++ CHAPTER-2/EpisodeTestDrive.java
@@ -0,0 +1,18 @@
+class Episode{
+	int seriesNumber;
+	int episodeNumber;
+	void skipIntro() {
+		System.out.println("Skipping intro...");
+	}
+	void skipToNext() {
+		System.out.println("Loading next episode...");
+	}
+}
+class EpisodeTestDrive {
+	public static void main(String[] args) {
+		Episode episode = new Episode();
+		episode.seriesNumber = 4;
+		episode.skipIntro();
+		episode.skipToNext();
+	}
+}
