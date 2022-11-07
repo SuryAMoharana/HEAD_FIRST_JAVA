b127acc756732fc2ce96893d0fab96a8b43b9824
Surya Moharana <suryachintu1994@gmail.com>
2022-11-07 23:14:00 +0530

    CHAPTER-12
    Practice Modified

--- CHAPTER-12/ForEach.java
+++ CHAPTER-12/ForEach.java
@@ -1,11 +1,13 @@
 import java.util.*;
+import java.util.stream.*;
 
 class ForEach{
 	public static void main(String[] args) {
 		List<Integer> numbers=List.of(1,2,3,4,5);
 
-		for(Integer num:numbers)
-		System.out.println(num);
+		Stream<Integer> stream=numbers.stream();
+
+		System.out.println(Arrays.toString(stream));
 
 	}
 }
