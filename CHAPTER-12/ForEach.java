0f9526b1661b5e0a0a34a2f2f06029d41b419ba4
Surya Moharana <suryachintu1994@gmail.com>
2022-11-07 22:46:48 +0530

    CHAPTER-12
    Exercise added

---
+++ CHAPTER-12/ForEach.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class ForEach{
+	public static void main(String[] args) {
+		List<String> colors=List.of("blue","black","red");
+
+		colors.forEach(clr -> System.out.println(clr));
+
+	}
+}
