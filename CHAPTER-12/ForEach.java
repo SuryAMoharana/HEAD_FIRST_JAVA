000428119e88e4995a45ba61682b4ae29951caec
Surya Moharana <suryachintu1994@gmail.com>
2022-11-08 10:01:06 +0530

    CHAPTER-12
    Exercise added

--- CHAPTER-12/ForEach.java
+++ CHAPTER-12/ForEach.java
@@ -3,11 +3,16 @@ import java.util.stream.*;
 
 class ForEach{
 	public static void main(String[] args) {
-		List<String> strings=List.of("I","am","a","List");
-
-		Stream<String> stream=strings.stream().sorted((a,b)->a.compareToIgnoreCase(b));
-
-		System.out.println(stream);
-
+		Runnable r = () -> System.out.println("Hi!");
+		Consumer<String> c = s ->System.out.println(s);
+		Supplier<String> s = () ->System.out.println("Some string");
+		Consumer<String> c = (s1, s2) ->System.out.println(s1 + s2);
+		Runnable r = (String str) ->System.out.println(str);
+		Function<String, Integer> f = s ->s.length();
+		Supplier<String> s = () -> "Some string";
+		Consumer<String> c = s -> "String" + s;
+		Function<String, Integer> f = (int i) -> "i =" + i;
+		Supplier<String> s = s -> "Some string: " +s;
+		Function<String, Integer> f = (String s) ->s.length();
 	}
 }
